9cdbabe539d2ecfc39880387b0205b38802a8aea
rukonit <rukon-it@live.com>
2021-06-06 23:28:46 -0400

    Added Observable Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated= false;
+  private activatedSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+  this.activatedSub =  this.userService.activatedEmmiter.subscribe(
+      (data) => {
+        this.userActivated = data;
+      }
+    )
+  }
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
 }
